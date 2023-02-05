34cdc7dc9272970496ceffe8f765fc0f3cef505e
unaguna <k.izumi.ysk@gmail.com>
2023-02-05 20:26:42 +0900

    ✨ Factory with post-processing

--- ranog/factory/_base.py
+++ ranog/factory/_base.py
@@ -3,6 +3,7 @@ import typing as t
 from random import Random
 
 T = t.TypeVar("T")
+R = t.TypeVar("R")
 
 
 class Factory(ABC, t.Generic[T]):
@@ -24,3 +25,41 @@ class Factory(ABC, t.Generic[T]):
             weights=[1 - prob, prob],
             rnd=rnd,
         )
+
+    def post_process(self, post_process: t.Callable[[T], R]) -> "Factory[R]":
+        """Returns a factory whose result will be modified by `post_process`
+
+        Examples
+        --------
+        >>> import ranog
+        >>>
+        >>> factory = ranog.factory.randstr(length=9).post_process(lambda v: v + v[0])
+        >>>
+        >>> generated = factory.next()
+        >>>
+        >>> assert len(generated) == 10
+        >>> assert generated[0] == generated[-1]
+
+        Parameters
+        ----------
+        post_process
+            the mapping to modify the result
+
+        Returns
+        -------
+        A factory whose result will be modified by `post_process`.
+        """
+        return PostFactory(self, post_process)
+
+
+class PostFactory(Factory[R], t.Generic[T, R]):
+    _base_factory: Factory[T]
+    _post_process: t.Callable[[T], R]
+
+    def __init__(self, base_factory: Factory[T], post_process: t.Callable[[T], R]):
+        self._base_factory = base_factory
+        self._post_process = post_process
+
+    def next(self) -> R:
+        pre_result = self._base_factory.next()
+        return self._post_process(pre_result)

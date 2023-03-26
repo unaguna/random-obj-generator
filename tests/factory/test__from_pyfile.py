e691706ebbdd9eb99c579e10b8394e5bf2531ea1
K-izumi <k.izumi.ysk@gmail.com>
2023-03-26 16:37:26 +0900

    ✅ use pytest.fixture

--- tests/factory/test__from_pyfile.py
+++ tests/factory/test__from_pyfile.py
@@ -1,9 +1,8 @@
 import randog.factory
 
 
-def test__from_pyfile():
-    # TODO: ファイル指定を堅牢に
-    filepath = "./tests/resource/factory_def.py"
+def test__from_pyfile(resources):
+    filepath = resources.joinpath("factory_def.py")
     factory = randog.factory.from_pyfile(filepath)
 
     value = factory.next()

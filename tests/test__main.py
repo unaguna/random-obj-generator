e9745869643db2af42af6091b94e2ac0521d754e
K-izumi <k.izumi.ysk@gmail.com>
2023-04-15 15:37:32 +0900

    ✅ repeat generation in command line

--- tests/test__main.py
+++ tests/test__main.py
@@ -1,6 +1,8 @@
 import sys
 from unittest.mock import patch
 
+import pytest
+
 import randog.__main__
 
 
@@ -21,4 +23,21 @@ def test__main__option_repr(capfd, resources):
 
         out, err = capfd.readouterr()
         assert out == "'aaa'\n"
+        assert err == ""
+
+
+@pytest.mark.parametrize(
+    ("option", "count"),
+    [
+        ("--repeat", 3),
+        ("-r", 2),
+    ],
+)
+def test__main__option_repeat(capfd, resources, option, count):
+    args = ["randog", option, str(count), str(resources.joinpath("factory_def.py"))]
+    with patch.object(sys, "argv", args):
+        randog.__main__.main()
+
+        out, err = capfd.readouterr()
+        assert out == "aaa\n" * count
         assert err == ""

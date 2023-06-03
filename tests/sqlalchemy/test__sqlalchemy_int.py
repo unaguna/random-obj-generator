6f24edd80d0a4d7c107de6ca083d7d6e2ab8b038
K-izumi <k.izumi.ysk@gmail.com>
2023-06-03 17:34:06 +0900

    ✅ factory from sqlalchemy (BigInteger)

--- tests/sqlalchemy/test__sqlalchemy_int.py
+++ tests/sqlalchemy/test__sqlalchemy_int.py
@@ -55,3 +55,41 @@ def test__sqlalchemy_custom__integer2(my_base, nullable):
         assert value_types == {int, type(None)}
     else:
         assert value_types == {int}
+
+
+@pytest.mark.require_sqlalchemy(1, 2)
+@pytest.mark.parametrize("nullable", (True, False))
+def test__sqlalchemy_custom__biginteger(nullable):
+    example = sqlalchemy.Column("col", sqlalchemy.BigInteger, nullable=nullable)
+    factory = randog.factory.from_example(example, custom_func=sqlalchemy_custom)
+
+    value_types = set(map(lambda _: type(factory.next()), range(200)))
+
+    if nullable:
+        assert value_types == {int, type(None)}
+    else:
+        assert value_types == {int}
+
+
+@pytest.mark.require_sqlalchemy(2)
+@pytest.mark.parametrize("nullable", (True, False))
+def test__sqlalchemy_custom__biginteger2(my_base, nullable):
+    import sqlalchemy.orm
+
+    class MyModel(my_base):
+        __tablename__ = "my_table"
+
+        id: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(primary_key=True)
+        field: sqlalchemy.orm.Mapped[int] = sqlalchemy.orm.mapped_column(
+            sqlalchemy.types.BIGINT, nullable=nullable
+        )
+
+    example = MyModel.field
+    factory = randog.factory.from_example(example, custom_func=sqlalchemy_custom)
+
+    value_types = set(map(lambda _: type(factory.next()), range(200)))
+
+    if nullable:
+        assert value_types == {int, type(None)}
+    else:
+        assert value_types == {int}

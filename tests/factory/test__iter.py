a57f4877b6644cc17eda2cc42c8db7cd16c87261
K-izumi <k.izumi.ysk@gmail.com>
2023-07-29 21:00:36 +0900

    ✅ stop iteration when inner factory raises StopIteration

--- tests/factory/test__iter.py
+++ tests/factory/test__iter.py
@@ -6,12 +6,43 @@ import pytest
 import randog.factory
 
 
+def _only_2_times():
+    _only_2_times.count += 1
+    if _only_2_times.count >= 3:
+        raise StopIteration
+    return 0
+
+
+_only_2_times.count = 0
+
+
 def test__iter():
     factory = randog.factory.by_iterator(itertools.count(0))
     generated = list(factory.iter(10))
 
     assert isinstance(generated, list)
     assert generated == list(range(10))
+
+
+@pytest.mark.parametrize(
+    "factory",
+    [
+        randog.factory.by_callable(_only_2_times),
+        randog.factory.by_iterator(iter(range(1, 3))),
+        randog.factory.randdict(
+            a=randog.factory.by_iterator(iter(range(1, 3))),
+            b=randog.factory.randint(0, 10),
+        ),
+        randog.factory.randlist(
+            randog.factory.by_iterator(iter(range(1, 3))),
+            randog.factory.randint(0, 10),
+        ),
+    ],
+)
+def test__iter__stop_iteration(factory):
+    generated = list(factory.iter(10))
+
+    assert len(generated) == 2
 
 
 def test__iter__regenerate():

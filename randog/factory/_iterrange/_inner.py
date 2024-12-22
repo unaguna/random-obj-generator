e6a940a7e45d036048dc686ef6edde63e0b57434
K-izumi <k.izumi.ysk@gmail.com>
2024-12-22 15:01:46 +0900

    🔥 remove unused function

--- randog/factory/_iterrange/_inner.py
+++ randog/factory/_iterrange/_inner.py
@@ -105,26 +105,3 @@ class Iterrange(Factory[F_T], abc.ABC, t.Generic[F_T, S]):
                         "This factory no longer generates values."
                     )
                     break
-
-
-def _iterrange(
-    initial_value, minimum, maximum, step, resume_value, *, cyclic: bool
-) -> t.Iterator:
-    next_value = initial_value
-    while True:
-        yield next_value
-        next_value += step
-
-        if next_value > maximum or next_value < minimum:
-            if cyclic:
-                logger.debug(
-                    "iterrange() has reached its maximum value and resumes "
-                    f"from {resume_value}"
-                )
-                next_value = resume_value
-            else:
-                logger.debug(
-                    "iterrange() has reached its maximum value. "
-                    "This factory no longer generates values."
-                )
-                break

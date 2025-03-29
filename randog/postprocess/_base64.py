c45b11ec6f50040a1385ce0cd677f32aaf4205ce
K-izumi <k.izumi.ysk@gmail.com>
2025-03-29 16:35:17 +0900

    ♻ --base64 in byfile mode

--- randog/postprocess/_base64.py
+++ randog/postprocess/_base64.py
@@ -12,33 +12,23 @@ class Base64PostProcess(BasePostProcess):
         return (str,)
 
     def __call__(self, pre_value: t.Any) -> t.Any:
-        if isinstance(pre_value, t.Mapping):
-            return {
-                key: (
-                    self(value)
-                    if isinstance(value, (t.Sequence, t.Mapping, bytes))
-                    and not isinstance(value, str)
-                    else value
-                )
-                for key, value in pre_value.items()
-            }
-        elif isinstance(pre_value, t.Sequence) and not isinstance(
-            pre_value, (str, bytes)
-        ):
-            next_type = tuple
-            if isinstance(pre_value, t.MutableSequence):
-                next_type = list
+        next_val = _b64encode_or_asis(pre_value)
+        if pre_value is next_val:
+            raise TypeError()
 
-            return next_type(
-                [
-                    (
-                        self(value)
-                        if isinstance(value, (t.Sequence, t.Mapping, bytes))
-                        and not isinstance(value, str)
-                        else value
-                    )
-                    for value in pre_value
-                ]
-            )
+        return next_val
 
+
+def _b64encode_or_asis(pre_value: t.Any) -> t.Any:
+    if isinstance(pre_value, bytes):
         return base64.b64encode(pre_value).decode("utf-8")
+    elif isinstance(pre_value, t.Mapping):
+        return {key: _b64encode_or_asis(value) for key, value in pre_value.items()}
+    elif isinstance(pre_value, t.Sequence) and not isinstance(pre_value, str):
+        next_type = tuple
+        if isinstance(pre_value, t.MutableSequence):
+            next_type = list
+
+        return next_type(_b64encode_or_asis(value) for value in pre_value)
+    else:
+        return pre_value
